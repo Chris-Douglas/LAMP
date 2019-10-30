8d913ec8ee8a65b79729da75b603d945385bcb7b
Christopher-0 <Douglas.Christopher.Paul@gmail.com>
2019-10-31 10:23:22 +1100

    test

--- php/PHP-formVal02.php
+++ php/PHP-formVal02.php
@@ -32,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
                 $emailErr = "Invalid email format";
             }
         }  
+        
 }
 function test_input($data) {
     $data = trim($data);
