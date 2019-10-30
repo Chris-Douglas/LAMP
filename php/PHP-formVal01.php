82a18ca6cbf588288b34cacf1dd0dc04a1a622e9
Christopher-0 <Douglas.Christopher.Paul@gmail.com>
2019-10-31 10:20:58 +1100

    test

--- php/PHP-formVal01.php
+++ php/PHP-formVal01.php
@@ -38,3 +38,51 @@ echo "<p> Your title is $strTitle </p>";
 }
 ?>
 </html>
+
+<html>
+<?php
+//Define variable and set to empty
+$nameErr = $surNameErr = $emailErr = $colorErr = $sizeErr = $numOfShirtErr = "";
+$name = $surName = $email = $color = $size = $numOfShirt = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty($_POST["name"])){
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        //Check id name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)){
+            $nameErr = "Only letters and white space allowed"; 
+        }
+    }
+    if(empty($_POST["surname"])){
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["surname"]);
+        //Check id name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)){
+            $nameErr = "Only letters and white space allowed";
+    if(empty($_POST["email"])){
+        $emailErr = "Email is required";
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $emailErr = "Invalid email format";
+        }
+    }
+    
+    
+}
+?>
+<form action='<?php echo $_SERVER["PHP_SELF"]; ?>' method='post'>
+Your name: <input type="text" name="name" /> 
+<span class="error">* <?php echo $nameErr;?></span>
+<br>
+Your surname: <input type="text" name="surname" /> 
+<span class="error">* <?php echo $surNameErr;?></span>
+<br>
+Your email: <input type="text" name="email" /> 
+<span class="error">* <?php echo $emailErr;?></span>
+<br>
+<input type = 'submit' name='submit' value='submit'/>
+</form>
+</html>
--- php/PHP-formVal02.php
+++ php/PHP-formVal02.php
@@ -1,8 +1,8 @@
 <html>
 <?php
 //Define variable and set to empty
-$nameErr = $surNameErr = $emailErr = $colorErr = $sizeErr = $numOfShirtErr = "";
-$name = $surName = $email = $color = $size = $numOfShirt = "";
+$nameErr =  $websiteErr = $emailErr = "";
+$name = $website = $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($_POST["name"])){
@@ -14,30 +14,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $nameErr = "Only letters and white space allowed"; 
         }
     }
-    if(empty($_POST["surname"])){
-        $nameErr = "Name is required";
-    } else {
-        $name = test_input($_POST["surname"]);
-        //Check id name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z ]*$/",$name)){
-            $nameErr = "Only letters and white space allowed";
+    if(empty($_POST["website"])){
+        $websiteErr = "Website Required";
+        } else {
+            $website = test_input($_POST["website"]);
+            //Validate Website
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                $websiteErr = "Invalid URL"; 
+            }  
+        }
     if(empty($_POST["email"])){
         $emailErr = "Email is required";
-        }
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $emailErr = "Invalid email format";
-        }
-    }
-    
-    
+        } else {
+            $email = test_input($_POST["email"]);
+            //Validate email
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $emailErr = "Invalid email format";
+            }
+        }  
+}
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 ?>
 <form action='<?php echo $_SERVER["PHP_SELF"]; ?>' method='post'>
-<p> Your name: <input type="text" name="name" /> </p>
-<p> Your surname: <input type="text" name="surname" /> </p>
-<p> Your email: <input type="text" name="email" /> </p>
-<p> Your prefered color: <input type="text" name="color" /> </p>
-<p>Number ordered: <input type="text" name="numOrdered"/> </p>
+Your name: <input type="text" name="name" /> 
+<span class="error">* <?php echo $nameErr;?></span>
+<br>
+Website: <input type="text" name="website" /> 
+<span class="error">* <?php echo $websiteErr;?></span>
+<br>
+Your email: <input type="text" name="email" /> 
+<span class="error">* <?php echo $emailErr;?></span>
+<br>
 <input type = 'submit' name='submit' value='submit'/>
 </form>
 </html>
